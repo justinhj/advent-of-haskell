a8ebf237c00355d3f4d66fc995a71f3cd3012b04
Justin Heyes-Jones <justinhj@gmail.com>
2024-12-01 22:47:06 -0800

    solve 1 1

--- problems/DayOne/PartOne.hs
+++ problems/DayOne/PartOne.hs
@@ -7,14 +7,17 @@ module DayOne.PartOne(Out, solution) where
 import Lib.Solution
 import Lib.Types
 import Helpers.Solution
+import Data.List (sort)
 
 -- | The type of the answer to this problem
 type Out = Int
 
 examples :: [(String, Out)]
-examples = [ -- ("input", 20),
+examples = [ ("input", 3714264),
              ("example", 11) ]
 
+-- TODO use the authors parsing here or is it overkill?
+-- TODO optionally use Result here
 parseLine :: ([Int], [Int]) -> String -> ([Int], [Int])
 parseLine (l1, l2) line = case map read (words line) :: [Int] of
   [n1, n2] -> (n1 : l1, n2 : l2)
@@ -24,9 +27,10 @@ parseAll :: String -> ([Int], [Int])
 parseAll input = foldl parseLine ([],[]) (lines input)
 
 solve :: ([Int], [Int]) -> Result Int
--- solve (l1,l2) = Right 0
-solve ([],[]) = Right 0
-solve (_,_) = Right 11 
+solve (l1,l2) = let s1 = sort l1
+                    s2 = sort l2
+                    in
+  Right (foldl (\acc (x, y) -> acc + abs (x - y)) 0 (zip s1 s2))
 
 -- | Solution for Day One, Part One
 solution:: AdventProblem Out
