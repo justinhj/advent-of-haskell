c14f51ce1d872a5db6ebb98cc05d549f27305822
Justin Heyes-Jones <justinhj@gmail.com>
2024-12-06 18:53:45 -0800

    failed solution needs tweaking

--- problems/DayTwo/PartTwo.hs
+++ problems/DayTwo/PartTwo.hs
@@ -6,14 +6,53 @@ module DayTwo.PartTwo(Out, solution) where
 
 import Lib.Solution
 import Lib.Types
-import Helpers.Solution
+import Control.Monad (foldM)
+import Data.Maybe (mapMaybe)
+import Text.Read (readMaybe)
 
 -- | The type of the answer to this problem
 type Out = Int
 
 examples :: [(String, Out)]
-examples = []
+examples = [ ("example", 4),
+              ("input", 334) ]
 
--- | Solution for Day Two, Part Two
+parseLine :: [[Int]] -> String -> Result [[Int]]
+parseLine nums line = let
+  these = mapMaybe readMaybe (words line)
+    in 
+  if null these 
+    then Left "Parse error"
+    else Right (these : nums)
+
+parseAll :: String -> Result [[Int]]
+parseAll input = foldM parseLine [] (lines input)
+
+removeNth :: Int -> [a] -> [a]
+removeNth n xs = take n xs ++ drop (n + 1) xs
+
+getRemovals :: [Int] -> [[Int]]
+getRemovals l = map (\n -> removeNth n l) [0..(length l-1)] 
+
+getDiffs :: [Int] -> [Int]
+getDiffs z = map (uncurry (-)) zipped
+  where 
+    zipped = zip z (tail z) 
+
+combineInputsWithRemovals :: [[Int]] -> [[Int]]
+combineInputsWithRemovals = concatMap (\x -> [x] ++ (getRemovals x))
+
+allSameSign :: [Int] -> Bool
+allSameSign [] = False
+allSameSign (x:xs) = all (\a -> signum a == signum x) xs
+
+allGradualChange :: [Int] -> Bool
+allGradualChange l = (minimum abs_vals >= 1) && (maximum abs_vals <=3)
+  where abs_vals = map abs l
+
+solve :: [[Int]] -> Result Int
+solve xss = (Right . length . filter (\xs -> allSameSign xs && allGradualChange xs) . map getDiffs) (combineInputsWithRemovals xss)
+
+-- | Solution for Day Two, Part One
 solution:: AdventProblem Out
-solution = adventOfCode examples (nyi "Parsing" :: String -> Result Out) (nyi "Solution")
+solution = adventOfCode examples parseAll solve
