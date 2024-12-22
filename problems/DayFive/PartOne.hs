2c19af0c2dcfa456e046362caa2ffc311ee62a2c
Justin Heyes-Jones <justinhj@gmail.com>
2024-12-22 15:13:00 -0800

    tests working step 1

--- problems/DayFive/PartOne.hs
+++ problems/DayFive/PartOne.hs
@@ -2,22 +2,45 @@
 module: DayFive.PartOne
 description: Advent of Code, Day Five, Part One
 -}
-module DayFive.PartOne(Out, solution, parse) where
+module DayFive.PartOne(Out, solution, parseInput, parseLines) where
 
 import Lib.Solution
-import Lib.Types
+import Lib.Types hiding (Parser)
+import Lib.Types hiding (Parser)
 import Data.List.Split (splitOn)
 import Helpers.Solution
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+import Text.Parsec
+import Text.Parsec.String
 
 -- | The type of the answer to this problem
 type Out = Int
 
+lineParser :: Parser (Int, Int)
+lineParser = do
+    left <- many1 digit
+    _ <- char '|'
+    right <- many1 digit
+    return (read left, read right)
+
+linesParser :: Parser [(Int, Int)]
+linesParser = many (lineParser <* newline)
+
+toMap :: [(Int, Int)] -> Map.Map Int (Set.Set Int)
+toMap = Map.fromListWith Set.union . map (\(a, b) -> (b, Set.singleton a))
+
+parseLines :: String -> Either ParseError (Map.Map Int (Set.Set Int))
+parseLines input = case parse linesParser "" input of
+    Left err -> Left err
+    Right pairs -> Right (toMap pairs)
+
 examples :: [(String, Out)]
 examples = [("test1", 143)]
 
-parse :: String -> Result Int
-parse input = Right 10
+parseInput :: String -> Result Int
+parseInput input = Right 10
 
 -- | Solution for Day Five, Part One
 solution:: AdventProblem Out
-solution = adventOfCode examples parse (nyi "Solution")
+solution = adventOfCode examples parseInput (nyi "Solution")
--- test/5/Test5.hs
+++ test/5/Test5.hs
@@ -1,10 +1,43 @@
 module Test5 (test5) where
 
+import DayFive.PartOne (parseInput, parseLines)
 import Test.HUnit
+import Data.Either (isRight)
+import Data.Map (lookup)
+import qualified Data.Set as Set
+import qualified Data.Map as Map
+
+test1 :: String
+test1 = "47|53\n\
+        \97|13\n\
+        \97|61\n\
+        \97|47\n\
+        \75|29\n\
+        \61|13\n\
+        \75|53\n\
+        \29|13\n\
+        \97|29\n\
+        \53|29\n\
+        \61|53\n\
+        \97|53\n\
+        \61|29\n\
+        \47|13\n\
+        \75|47\n\
+        \97|75\n\
+        \47|61\n\
+        \75|61\n\
+        \47|29\n\
+        \75|13\n\
+        \53|13\n"
+
+parsed = parseLines test1
+expectedSet_61 = Just $ Set.fromList [97,47,75]
 
 test5 :: Test
-test5 = TestCase (assertEqual "for (foo 3)," 8 (foo 3))
-
-foo :: Int -> Int
-foo x = x + 5
-
+test5 = TestList [
+    TestCase (assertBool "Parsed" (isRight parsed)),
+    TestCase $
+      case parsed of
+        Right m -> assertEqual "Correct set elements" expectedSet_61 (Map.lookup 61 m)
+        Left err -> assertFailure ("Parsing failed: " ++ show err)
+  ]
